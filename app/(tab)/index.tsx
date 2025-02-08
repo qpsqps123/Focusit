1895c92606859ee9eab57cd592b002625a10188d
qpsqps123 <qpsqps321@gmail.com>
2025-02-08 23:32:26 +0900

    apply yeonSung font to TodoList Tab

--- app/(tab)/index.tsx
+++ app/(tab)/index.tsx
@@ -1,6 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
+import { useFonts, YeonSung_400Regular } from '@expo-google-fonts/yeon-sung';
 
 export default function TodoList() {
+  let [fontsLoaded] = useFonts({
+    YeonSung_400Regular,
+  });
+
+ if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Todo App</Text>
@@ -14,5 +22,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#69247c",
     alignItems: "center",
   },
-  title: { fontSize: 24, color: "#FAC67A", marginTop: 80, }
+  title: { fontSize: 24, color: "#FAC67A", marginTop: 80, fontFamily: "YeonSung_400Regular" }
 })
