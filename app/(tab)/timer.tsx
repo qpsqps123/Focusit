c635b512d42f93b294c915829d4d4d7bdefc839d
qpsqps123 <qpsqps321@gmail.com>
2025-02-09 23:58:21 +0900

    apply yeonSung font to Timer Tab

--- app/(tab)/timer.tsx
+++ app/(tab)/timer.tsx
@@ -1,6 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
+import { useFonts, YeonSung_400Regular } from '@expo-google-fonts/yeon-sung';
 
 export default function Timer() {
+  let [fontsLoaded] = useFonts({
+    YeonSung_400Regular,
+  });
+
+ if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Timer</Text>
@@ -14,5 +22,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#69247c",
     alignItems: "center",
   },
-  title: { fontSize: 24, color: "#FAC67A", marginTop: 80, }
+  title: { fontSize: 24, color: "#FAC67A", marginTop: 80, fontFamily: "YeonSung_400Regular" }
 })
