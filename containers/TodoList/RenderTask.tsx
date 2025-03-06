74de56dec76f21866d7ed677b62591b046b564a6
qpsqps123 <qpsqps321@gmail.com>
2025-03-06 15:30:36 +0900

    add toggling for task menu visibility

--- containers/TodoList/RenderTask.tsx
+++ containers/TodoList/RenderTask.tsx
@@ -5,8 +5,11 @@ import { TodoListProps, Tasks } from "./types";
 import { useFonts } from "expo-font";
 import { Jua_400Regular } from "@expo-google-fonts/jua";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useState } from "react";
 
 export default function RenderTask({ tasks, setTasks }: TodoListProps) {
+  const [openedTaskMenuIds, setOpenedTaskMenuIds] = useState<Array<String>>([]);
+
   const [fontsLoaded, fontsError] = useFonts({
     Jua_400Regular,
   });
@@ -18,42 +21,62 @@ export default function RenderTask({ tasks, setTasks }: TodoListProps) {
     setData("tasks", JSON.stringify(updatedTasks));
   };
 
+  const handleMenuSwitch = (taskId: string) => {
+    if (openedTaskMenuIds.includes(taskId)) {
+      const filteredIds = openedTaskMenuIds.filter(
+        (openedTaskMenuId) => openedTaskMenuId !== taskId,
+      );
+      setOpenedTaskMenuIds(filteredIds);
+    } else {
+      setOpenedTaskMenuIds([...openedTaskMenuIds, taskId]);
+    }
+  };
+
   if (!fontsLoaded && !fontsError) {
     return null;
   }
 
   return (
-    <View style={styles.renderTaskContainer}>
+    <View style={styles.renderTaskWrapper}>
       <FlatList
         data={tasks}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <Pressable
-            style={({ pressed }) => [
-              styles.renderTaskBtn,
-              { opacity: pressed ? 0.7 : 1 },
-            ]}
-            onPress={() => handleRemoveTask(item)}
-          >
-            <View style={styles.renderTask}>
-              <Text style={styles.renderTaskText}>{item.task}</Text>
-              <Pressable>
-                <MaterialCommunityIcons
-                  name="menu-down"
-                  size={24}
-                  color="black"
-                />
-              </Pressable>
+          <View style={styles.renderTaskContainer}>
+            <Pressable
+              style={({ pressed }) => [
+                styles.renderTaskBtn,
+                { opacity: pressed ? 0.7 : 1 },
+              ]}
+            >
+              <View style={styles.renderTask}>
+                <Text style={styles.renderTaskText}>{item.task}</Text>
+                <Pressable
+                  onPress={() => handleMenuSwitch(item.id)}
+                  style={styles.renderTaskMenuSwitch}
+                >
+                  <MaterialCommunityIcons
+                    name="menu-down"
+                    size={28}
+                    color="black"
+                  />
+                </Pressable>
+              </View>
+            </Pressable>
+            {openedTaskMenuIds.includes(item.id) && (
               <View style={styles.renderTaskMenu}>
                 <Pressable style={styles.renderTaskMenuBtns}>
                   <Text style={styles.renderTaskMenuText}>Edit</Text>
                 </Pressable>
-                <Pressable style={styles.renderTaskMenuBtns}>
+                <Pressable
+                  style={styles.renderTaskMenuBtns}
+                  onPress={() => handleRemoveTask(item)}
+                >
                   <Text style={styles.renderTaskMenuText}>Delete</Text>
                 </Pressable>
               </View>
-            </View>
-          </Pressable>
+            )}
+          </View>
         )}
         contentContainerStyle={styles.renderTaskList}
       />
@@ -62,13 +85,16 @@ export default function RenderTask({ tasks, setTasks }: TodoListProps) {
 }
 
 const styles = StyleSheet.create({
-  renderTaskContainer: {
+  renderTaskWrapper: {
     flex: 1,
     marginTop: 80,
     width: "80%",
   },
   renderTaskList: {
     gap: 20,
+  },
+  renderTaskContainer: {
+    position: "relative",
   },
   renderTaskBtn: {
     backgroundColor: theme.$primary,
@@ -87,12 +113,17 @@ const styles = StyleSheet.create({
     lineHeight: 25,
     flex: 1,
   },
+  renderTaskMenuSwitch: {
+    width: 28,
+    height: 28,
+  },
   renderTaskMenu: {
     backgroundColor: theme.$white,
     borderRadius: 10,
     position: "absolute",
-    top: 28,
+    top: 50,
     right: 0,
+    zIndex: 3,
   },
   renderTaskMenuBtns: {
     paddingHorizontal: 30,
