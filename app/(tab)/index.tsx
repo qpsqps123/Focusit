0d18ab3488c3de5b3125db954520af744408282e
qpsqps123 <qpsqps321@gmail.com>
2025-04-01 23:47:42 +0900

    Fix splash screen bug ...
    - The background color of splash screen was not applied. Probably
    related to New Architecture.
    - Fixed by expo-splash-screen plugin.

--- app/(tab)/index.tsx
+++ app/(tab)/index.tsx
@@ -1,6 +1,21 @@
 import TodoList from "@/containers/TodoList";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function TodoListTab() {
+  useEffect(() => {
+    const loadResources = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+        await SplashScreen.hideAsync();
+      } catch (e) {
+        console.warn("Failed to hide splash screen:", e);
+      }
+    };
+
+    loadResources();
+  }, []);
+
   return (
     <>
       <TodoList />
