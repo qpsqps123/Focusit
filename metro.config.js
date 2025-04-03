1df8e504e309e863547f69a8246b4c054b1418fc
qpsqps123 <qpsqps321@gmail.com>
2025-04-03 20:09:53 +0900

    remove unused code

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,4 @@
-// Learn more https://docs.expo.dev/guides/monorepos
 const { getDefaultConfig } = require("expo/metro-config");
 
-// Create the default Expo config for Metro
-// This includes the automatic monorepo configuration for workspaces
-// See: https://docs.expo.dev/guides/monorepos/#automatic-configuration
 const config = getDefaultConfig(__dirname);
 module.exports = config;
